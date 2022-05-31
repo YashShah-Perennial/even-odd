9f68624986af9f3a257c5a78ec0a55deb7956493
Yash <yash.shah@perennialsys.com>
2022-05-31 10:47:39 +0530

    odd code

--- Even_odd/src/Even_odd.java
+++ Even_odd/src/Even_odd.java
@@ -3,11 +3,11 @@ public class Even_odd
     int array []={1,2,3,4,5}; // initializing array
         int loop;
 
-        public void Even_position()
+        public void odd_position()
         {
             for (loop=0;loop< array.length;loop++)// loop for travelling
             {
-                if (loop%2==0)// logic of even
+                if (loop%2!=0)// logic of even
                 {
                     System.out.println(loop + "  "+array[loop]);
                 }
@@ -16,7 +16,7 @@ public class Even_odd
 
         public static void main(String[] args) {
         Even_odd object=new Even_odd();
-        object.Even_position();
+        object.odd_position();
     }
 
     }
